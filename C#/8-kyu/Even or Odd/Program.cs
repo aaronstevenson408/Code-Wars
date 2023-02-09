a29abb9d86a46df63d94cf9b22b0a8c155a2e1ce
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2023-02-08 21:44:25 -0800

    even or odd
    ugly solution, but it works against all test cases

---
+++ C#/8-kyu/Even or Odd/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] expectedOutputs = { "Even", "Odd", "Even", "Odd", "Odd" };
+
+            int[] inputs = { 2, 1, 0, 7, -1 };
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                string output = SolutionClass.EvenOrOdd(inputs[i]);
+                Console.WriteLine("Input: " + inputs[i] + ", Output: " + output + ", Expected: " + expectedOutputs[i]);
+
+                if (output == expectedOutputs[i])
+                {
+                    Console.WriteLine("Success");
+                }
+                else
+                {
+                    Console.WriteLine("Failed");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+    public class SolutionClass
+    {
+        public static string EvenOrOdd(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return "Even";
+            }
+            else 
+            {
+                return "Odd";
+            }
+        }
+    }
+}
