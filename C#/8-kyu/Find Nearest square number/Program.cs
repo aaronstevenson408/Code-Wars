86da7d5ab1fed1aff5f9df89c541548e14230e47
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2023-02-08 22:14:07 -0800

    initial full solve
    this completes the kata requirements

---
+++ C#/8-kyu/Find Nearest square number/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Your task is to find the nearest square number, nearest_sq(n) or nearestSq(n), of a positive integer n.
+
+For example, if n = 111, then nearest\_sq(n) (nearestSq(n)) equals 121, since 111 is closer to 121, the square of 11, than 100, the square of 10.
+
+If the n is already the perfect square (e.g. n = 144, n = 81, etc.), you need to just return n.
+
+Good luck :)
+
+Check my other katas:
+
+Alphabetically ordered
+
+Case-sensitive!
+
+Not prime numbers
+
+*/
+using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.NearestSq(1) == 1);
+            Console.WriteLine(Kata.NearestSq(2) == 1);
+            Console.WriteLine(Kata.NearestSq(10) == 9);
+            Console.WriteLine(Kata.NearestSq(111) == 121);
+            Console.WriteLine(Kata.NearestSq(9999) == 10000);
+        }
+    }
+
+    public static class Kata
+    {
+        public static int NearestSq(int n)
+        {
+            double number = Math.Pow(Math.Round(Math.Sqrt(n)), 2);
+            Console.WriteLine(number);
+            return Convert.ToInt32(number);
+
+            throw new NotImplementedException("NearestSq is not implemented.");
+        }
+    }
+}
