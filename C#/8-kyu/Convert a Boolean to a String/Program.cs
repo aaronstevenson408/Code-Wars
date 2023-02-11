6e0f4e1fe94195bdf42052a89f7703731391b74a
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2023-02-10 21:34:25 -0800

    booltostring

---
+++ C#/8-kyu/Convert a Boolean to a String/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.*/
+
+using System;
+
+public class kata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing MyTest: " + (kata.BooleanToString(true) == "True"));
+            Console.WriteLine("Testing MyTest: " + (kata.BooleanToString(false) == "False"));
+        }
+    }
+    public static string BooleanToString(bool b)
+    {
+        if (b)
+        {
+            return "True";
+        }
+        if (!b)
+        {
+            return "False";
+        }
+        else
+        {
+            return "Error";
+        }
+    }
+}
