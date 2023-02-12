a389ce76006d3cdd70a28f278f56650719739996
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2023-02-11 22:08:43 -0800

    price of mangos incomplete

---
+++ C#/8-kyu/Price of Mangoes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing BasicTests: " + (Kata.Mango(3, 3) == 6));
+            Console.WriteLine("Testing BasicTests: " + (Kata.Mango(9, 5) == 30));
+        }
+    }
+    public class Kata
+    {
+        public static int Mango(int quantity, int price)
+            // needs to find multiples of 3 , charge for2 , then charge for the remaining ones
+        {
+            decimal saleQuantity = quantity / 2;
+            decimal notSale = quantity%2;
+            decimal total = (Math.Floor(saleQuantity)+notSale)*price;
+            return Decimal.ToInt32(total);
+        }
+    }
+}
